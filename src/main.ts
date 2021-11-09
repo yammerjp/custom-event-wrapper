1901a69a2a674935409d922c89cc64de5c399ab7
Keisuke Nakayama <me@yammer.jp>
2021-11-10 08:50:15 +0900

    Distinguish Event and CustomEvent

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
 
 console.log('hello');
+
+addEventListener('event-name', (event: Event) => {
+  console.log('event listener is running');
+  console.log('detail:')
+  if (event instanceof CustomEvent) {
+    console.log(event.detail)
+  } else {
+    console.error('this is not custom event');
+  }
+});
+
+const customEvent = new CustomEvent('event-name', { detail: {
+  any: "object"
+} });
+
+dispatchEvent(customEvent);
+
+dispatchEvent(new Event('event-name'))
